6bbae81dabc429fc98200029e472c55de43f888e
horamupa <horamupa@gmail.com>
2023-03-14 12:13:54 +0400

    Initial Commit

---
+++ EffectiveMobileTestApp/EffectiveMobileTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestAppApp.swift
+//  EffectiveMobileTestApp
+//
+//  Created by MM on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
